d4ed77f30625a3547283dc95a80a229a1ad9efca
thevalidator <the.validator@yandex.ru>
2023-05-23 00:53:10 +0300

    Update CyberSentinelBot.java

--- core/src/main/java/ru/thevalidator/core/controller/CyberSentinelBot.java
+++ core/src/main/java/ru/thevalidator/core/controller/CyberSentinelBot.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2023 thevalidator
  */
-
 package ru.thevalidator.core.controller;
 
 import java.util.List;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -20,7 +20,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
  */
 @Component
 public class CyberSentinelBot extends TelegramLongPollingBot {
-    
+
     @Value("${bot.name}")
     private String botName;
     private static final Logger logger = LogManager.getLogger(CyberSentinelBot.class);
@@ -31,26 +31,50 @@ public class CyberSentinelBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(">> " + botName + " - " + update.getMessage().getText());
-        String chatId = String.valueOf(update.getMessage().getChatId());
-        Integer msgId = update.getMessage().getMessageId();
-        //logger.trace("<< " + update.getMessage().getText());
-        logger.trace(update.toString());
-        //SendMessage msg = new SendMessage(String.valueOf(update.getMessage().getChatId()), "Sasai");
-        DeleteMessage msg = new DeleteMessage(chatId, msgId);
-        try {
-            
-            execute(msg);
-        } catch (TelegramApiException ex) {
-            logger.error(ex.getMessage());
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+            String chatId = String.valueOf(update.getMessage().getChatId());
+            SendMessage msg = null;
+            if (text.equalsIgnoreCase("юфуфус")) {
+                msg = new SendMessage(chatId, "Эй, Политолог, расскажи нам новости!");
+            } else if (text.equalsIgnoreCase("чуйбараш")) {
+                msg = new SendMessage(chatId, "Чуй, скинь фотку сисек кого-нибудь из своих бывших");
+            }
+
+            if (msg != null) {
+                try {
+                    execute(msg);
+                    //execute(emsg);
+                } catch (TelegramApiException ex) {
+                    logger.error(ex.getMessage());
+                }
+            }
+
         }
+////        System.out.println(">> " + botName + " - " + update.getMessage().getText());
+////        String chatId = String.valueOf(update.getMessage().getChatId());
+////        Integer msgId = update.getMessage().getMessageId();
+////        //logger.trace("<< " + update.getMessage().getText());
+////        logger.trace(update.toString());
+////        //SendMessage msg = new SendMessage(chatId, "Sasai");
+////        DeleteMessage msg = new DeleteMessage(chatId, msgId);
+////        //EditMessageText emsg = new EditMessageText(chatId, msgId, chatId, chatId, botName, Boolean.FALSE, replyMarkup, entities)
+//////        EditMessageText emsg = new EditMessageText();
+//////        emsg.setChatId(chatId);
+//////        emsg.setMessageId(msgId);
+//////        emsg.setText("Corrected");
+////        try {
+////            execute(msg);
+////            //execute(emsg);
+////        } catch (TelegramApiException ex) {
+////            logger.error(ex.getMessage());
+////        }
     }
 
 //    @Override
 //    public void onUpdatesReceived(List<Update> updates) {
 //        super.onUpdatesReceived(updates);
 //    }
-
     @Override
     public String getBotUsername() {
         return botName;
