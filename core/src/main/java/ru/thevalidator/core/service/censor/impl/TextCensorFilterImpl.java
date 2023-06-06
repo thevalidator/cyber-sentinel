8c49dec25bf3c64da8f068de294322c4c4c3bb8c
thevalidator <the.validator@yandex.ru>
2023-06-06 22:05:02 +0300

    Create TextCensorFilterImpl.java

---
+++ core/src/main/java/ru/thevalidator/core/service/censor/impl/TextCensorFilterImpl.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.core.service.censor.impl;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import ru.thevalidator.core.service.censor.TextCensorFilterService;
+import ru.thevalidator.core.util.ExceptionUtil;
+
+@Service
+public class TextCensorFilterImpl implements TextCensorFilterService {
+
+    private static final Logger logger = LogManager.getLogger(TextCensorFilterImpl.class);
+    private Set<String> swearWordsDictionary;
+
+    public TextCensorFilterImpl() {
+        this.swearWordsDictionary = new HashSet<>();
+        try (Reader reader = new FileReader("censor.dict", Charset.forName("UTF-8")); BufferedReader br = new BufferedReader(reader)) {
+            String line;
+            while ((line = br.readLine()) != null && !line.isBlank()) {
+                swearWordsDictionary.add(line);
+            }
+        } catch (Exception e) {
+            logger.error(ExceptionUtil.getFormattedDescription(e));
+        }
+    }
+
+    @Override
+    public String getFilteredText(String text) {
+        StringBuilder censoredText = new StringBuilder(text.length());
+        StringBuilder word = new StringBuilder();
+
+        for (char c: text.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                word.append(c);
+            } else {
+                String checked = checkAndFilterWord(word.toString());
+                censoredText.append(checked);
+                censoredText.append(c);
+                word.setLength(0);
+            }
+        }
+
+        if (!word.isEmpty()) {
+            censoredText.append(checkAndFilterWord(word.toString()));
+        }
+
+        String censored = censoredText.toString();
+        return censored.equals(text) ? null : censored;
+    }
+
+    private String checkAndFilterWord(String word) {
+        if (swearWordsDictionary.contains(word.toLowerCase())) {
+            StringBuilder sb = new StringBuilder(word.length());
+            for (int i = 0; i < word.length(); i++) {
+                sb.append("*");
+            }
+            return sb.toString();
+        }
+        return word;
+    }
+
+}
