f58a1009e8946c1c3d412d5aefb80166c73206e0
thevalidator <the.validator@yandex.ru>
2023-06-17 00:21:40 +0300

    Create RootApi.java

---
+++ core/src/main/java/ru/thevalidator/core/service/api/RootApi.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.core.service.api;
+
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface RootApi extends AbstractNativeApi {
+
+}
