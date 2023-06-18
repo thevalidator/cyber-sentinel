2173fa9516d9745979de44b150909d7683833f88
thevalidator <the.validator@yandex.ru>
2023-06-18 18:38:04 +0300

    Create SwearWordsService.java

---
+++ core/src/main/java/ru/thevalidator/core/service/censor/SwearWordsService.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.core.service.censor;
+
+import java.util.List;
+import ru.thevalidator.core.entity.SwearWord;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface SwearWordsService {
+    
+    SwearWord getById(int id);
+    
+    List<String> getAllWordsFilteredByCategory(int category);
+
+    void save(SwearWord word);
+
+    void saveAll(List<SwearWord> words);
+    
+}
