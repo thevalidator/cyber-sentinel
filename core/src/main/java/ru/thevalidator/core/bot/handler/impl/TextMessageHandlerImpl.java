f5200a49362c7525fee20e5726390bf16eeccda5
thevalidator <the.validator@yandex.ru>
2023-06-18 13:54:55 +0300

    forwarded message owner's id feature

--- core/src/main/java/ru/thevalidator/core/bot/handler/impl/TextMessageHandlerImpl.java
+++ core/src/main/java/ru/thevalidator/core/bot/handler/impl/TextMessageHandlerImpl.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.Serializable;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.ParseMode;
@@ -24,11 +25,15 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
 
     private final TextCensorFilterService filterService;
     private final RootApi apiService;
+    private final String adminId;
 
     @Autowired
-    public TextMessageHandlerImpl(TextCensorFilterService filterService, RootApi apiService) {
+    public TextMessageHandlerImpl(TextCensorFilterService filterService,
+            RootApi apiService,
+            @Value("${bot.admin.id}") String adminId) {
         this.filterService = filterService;
         this.apiService = apiService;
+        this.adminId = adminId;
     }
 
     @Override
@@ -36,25 +41,24 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
 
         String text = message.getText();
         String chatId = String.valueOf(message.getChatId());
-        
+
         //System.out.println(">>>> " + text);
+        //System.out.println(">>>> " + message.toString());
         SendMessage messageResponse = null;
-        if (text.equalsIgnoreCase(ID.getName())) {
+        if (text.startsWith("/") && String.valueOf(message.getFrom().getId()).equals(adminId)) {
+            messageResponse = new SendMessage(chatId, "Unsupported yet");
+        } else if (text.equalsIgnoreCase(ID.getName())) {
             String botResponseText;
             if (message.isReply()) {
                 message = message.getReplyToMessage();
             }
-            var userId = message.getFrom().getId();
-            var userName = message.getFrom().getUserName();
-            var userFirstName = message.getFrom().getFirstName();
-            var userLastName = message.getFrom().getLastName();
-            var userLang = message.getFrom().getLanguageCode();
-            botResponseText = String.format("id: %d\nusername: %s\nfirst name: %s\nlast name: %s\nlang: %s",
-                    userId, userName, userFirstName, userLastName, userLang);
+            User user = message.getFrom();
+            User forwardedUser = message.getForwardFrom();
+            botResponseText = generateResponseMessageText(user, forwardedUser);
             messageResponse = new SendMessage(chatId, botResponseText);
-        } else if (text.startsWith(IP.getName())) {
+        }else if (text.startsWith(IP.getName())) {
             String ipAdress = text.substring(3).trim();
-            String result = ipAdress.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}") 
+            String result = ipAdress.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
                     ? apiService.getIPGeoLocationData(ipAdress).toString()
                     : "Некорректно введен ip адрес";
             messageResponse = new SendMessage(chatId, result);
@@ -79,12 +83,30 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
                 response.add(deleteMessageResponse);
             }
         }
-        
+
         if (messageResponse != null) {
             response.add(messageResponse);
         }
-        
 
+    }
+
+    private String generateResponseMessageText(User user, User forwardedUser) {
+        String userInfo = getUserInfo(user);
+        if (forwardedUser != null) {
+            return userInfo + "\n\nFORWARDED FROM:\n" + getUserInfo(forwardedUser);
+        } else {
+            return userInfo;
+        }
+    }
+
+    private String getUserInfo(User user) {
+        var userId = user.getId();
+        var userName = user.getUserName();
+        var userFirstName = user.getFirstName();
+        var userLastName = user.getLastName();
+        var userLang = user.getLanguageCode();
+        return String.format("id: %d\nusername: %s\nfirst name: %s\nlast name: %s\nlang: %s",
+                    userId, userName, userFirstName, userLastName, userLang);
     }
 
 }
