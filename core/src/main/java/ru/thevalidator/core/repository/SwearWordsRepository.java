c08278463420f636237a04f63161ad70be06fe0f
thevalidator <the.validator@yandex.ru>
2023-06-19 03:29:29 +0300

    filter category methods added

--- core/src/main/java/ru/thevalidator/core/repository/SwearWordsRepository.java
+++ core/src/main/java/ru/thevalidator/core/repository/SwearWordsRepository.java
@@ -5,6 +5,7 @@ package ru.thevalidator.core.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -22,5 +23,19 @@ public interface SwearWordsRepository extends JpaRepository<SwearWord, Integer> 
             + "WHERE category >= (:category);"
             , nativeQuery = true)
     List<String> findAllWordsByCategory(@Param("category") int category);
+
+    @Query(value = ""
+            + "SELECT category "
+            + "FROM public.category "
+            + "WHERE id=1;"
+            , nativeQuery = true)
+    Integer findFilterCategory();
+    
+    @Modifying
+    @Query(value = "UPDATE public.category "
+            + "SET category=(:category) "
+            + "WHERE id=1;"
+            , nativeQuery = true)
+    void saveFilterCategory(@Param("category") int category);
     
 }
