b3a2466506fcb689e7c73cc0f07ac637c109bf6d
thevalidator <the.validator@yandex.ru>
2023-06-19 03:33:51 +0300

    Update TextCensorFilterService.java

--- core/src/main/java/ru/thevalidator/core/service/censor/TextCensorFilterService.java
+++ core/src/main/java/ru/thevalidator/core/service/censor/TextCensorFilterService.java
@@ -4,11 +4,19 @@
 
 package ru.thevalidator.core.service.censor;
 
+import ru.thevalidator.core.entity.SwearWord;
+
 /**
  * @author thevalidator <the.validator@yandex.ru>
  */
 public interface TextCensorFilterService {
     
     String getFilteredText(String text);
+    
+    void addWordToFilter(SwearWord word);
+    
+    void setFilterCategory(int category);
+
+    void updateWords();
 
 }
