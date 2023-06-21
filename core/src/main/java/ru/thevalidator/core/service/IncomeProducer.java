414c9a9b3d50b191d3e2a8eec3c99b0ed513ab44
thevalidator <the.validator@yandex.ru>
2023-06-21 22:47:15 +0300

    Create IncomeProducer.java

---
+++ core/src/main/java/ru/thevalidator/core/service/IncomeProducer.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.core.service;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface IncomeProducer {
+
+}
