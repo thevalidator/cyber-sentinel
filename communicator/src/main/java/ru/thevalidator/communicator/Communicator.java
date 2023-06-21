0a3609ee7849a5ff1ce767590ce415027e050837
thevalidator <the.validator@yandex.ru>
2023-06-21 22:54:02 +0300

    add comunicator module

---
+++ communicator/src/main/java/ru/thevalidator/communicator/Communicator.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.communicator;
+
+/**
+ *
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Communicator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
