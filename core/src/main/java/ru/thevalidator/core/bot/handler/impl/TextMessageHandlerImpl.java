db6d986d88f45d3d1e844516359a5e211e3bdfbf
thevalidator <the.validator@yandex.ru>
2023-06-23 00:38:29 +0300

    add new functionality and refactoring

--- core/src/main/java/ru/thevalidator/core/bot/handler/impl/TextMessageHandlerImpl.java
+++ core/src/main/java/ru/thevalidator/core/bot/handler/impl/TextMessageHandlerImpl.java
@@ -3,18 +3,20 @@
  */
 package ru.thevalidator.core.bot.handler.impl;
 
-import java.io.File;
 import java.io.Serializable;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.ActionType;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.ParseMode;
+import org.telegram.telegrambots.meta.api.methods.send.SendChatAction;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.User;
+import ru.thevalidator.core.bot.command.Command;
 import static ru.thevalidator.core.bot.command.Command.*;
 import ru.thevalidator.core.bot.handler.TextMessageHandler;
 import ru.thevalidator.core.entity.SwearWord;
@@ -27,14 +29,17 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
     private final TextCensorFilterService filterService;
     private final RootApi apiService;
     private final String adminId;
+    private final String groupChatId;
 
     @Autowired
     public TextMessageHandlerImpl(TextCensorFilterService filterService,
             RootApi apiService,
-            @Value("${bot.admin.id}") String adminId) {
+            @Value("${bot.admin.id}") String adminId,
+            @Value("${bot.groupchat.id}") String groupChatId) {
         this.filterService = filterService;
         this.apiService = apiService;
         this.adminId = adminId;
+        this.groupChatId = groupChatId;
     }
 
     @Override
@@ -43,13 +48,12 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
         String text = message.getText();
         String chatId = String.valueOf(message.getChatId());
 
-        //System.out.println(">>>> " + text);
-        //System.out.println(">>>> " + message.toString());
         SendMessage messageResponse = null;
+        boolean isAdmin = String.valueOf(message.getFrom().getId()).equals(adminId);
         if (text.startsWith("/")) {
             String botResponseText = null;
             boolean isReply = false;
-            if (String.valueOf(message.getFrom().getId()).equals(adminId)) {
+            if (isAdmin) {
                 try {
                     if (text.startsWith(ADD_SWEAR.getName())) {
                         Integer swearCategory = Integer.valueOf(text.substring(5, 6));
@@ -61,8 +65,31 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
                         Integer swearCategory = Integer.valueOf(text.substring(8, 9));
                         filterService.setFilterCategory(swearCategory);
                         botResponseText = "Установлена " + swearCategory + " категория фильтрации";
-                    } else if (text.startsWith(GET_CATEGORY.getName())) {
+                    } else if (text.equals(GET_CATEGORY.getName())) {
                         botResponseText = "Категория фильтрации: " + filterService.getFilterCategory();
+                    } else if (text.startsWith(SEND_MESSAGE.getName())) {
+                        chatId = groupChatId;
+                        response.add(createTypingChatAction(chatId));
+                        botResponseText = text.substring(6);
+                    } else if (text.equals(HELPME.getName())) {
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("Доступные команды:\n");
+                        for (Command cmd: Command.values()) {
+                            sb.append("-> ").append(cmd.getName()).append("\n");
+                        }
+                        botResponseText = sb.toString();
+                    } else if (text.equals(ID.getName())) {
+                        if (message.isReply()) {
+                            message = message.getReplyToMessage();
+                        }
+                        User user = message.getFrom();
+                        User forwardedUser = message.getForwardFrom();
+                        botResponseText = generateResponseMessageText(user, forwardedUser);
+                    } else if (text.startsWith(IP.getName())) {
+                        String ipAdress = text.substring(4).trim();
+                        botResponseText = ipAdress.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
+                                ? apiService.getIPGeoLocationData(ipAdress).toString()
+                                : "Некорректно введен ip адрес";
                     } else {
                         botResponseText = "Unsupported yet";
                     }
@@ -77,37 +104,12 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
             if (isReply) {
                 messageResponse.setReplyToMessageId(message.getMessageId());
             }
-        } else if (text.equalsIgnoreCase(ID.getName())) {
-            String botResponseText;
-            if (message.isReply()) {
-                message = message.getReplyToMessage();
-            }
-            User user = message.getFrom();
-            User forwardedUser = message.getForwardFrom();
-            botResponseText = generateResponseMessageText(user, forwardedUser);
-            messageResponse = new SendMessage(chatId, botResponseText);
-        } else if (text.startsWith(IP.getName())) {
-            String ipAdress = text.substring(3).trim();
-            String result = ipAdress.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
-                    ? apiService.getIPGeoLocationData(ipAdress).toString()
-                    : "Некорректно введен ip адрес";
-            messageResponse = new SendMessage(chatId, result);
-        } else if (text.equalsIgnoreCase("юфуфус")) {
-            messageResponse = new SendMessage(chatId, "Эй, Политолог, расскажи нам новости!");
-        } else if (text.equalsIgnoreCase("чуйбараш")) {
-            messageResponse = new SendMessage(chatId, "Чуй, куда дел черного, падла?");
-        } else if (text.equalsIgnoreCase(HELPME.getName())) {
-            File file = new File("/test.file");
-            messageResponse = new SendMessage(chatId, file.getAbsoluteFile().toString());
-            //messageResponse = new SendMessage(chatId, "Unsupported yet");
         } else {
             String censorFilteredMessage = filterService.getFilteredText(text);
             if (censorFilteredMessage != null) {
                 User user = message.getFrom();
-                String description = String.format("<code>Оригинальное сообщение от %s %s содержало бранные слова и было изменено:</code>\n\n",
-                        user.getFirstName(),
-                        user.getLastName() == null ? "" : user.getLastName());
-                messageResponse = new SendMessage(chatId, description + "<i>\"" + censorFilteredMessage + "\"</i>");
+                String description = getDescription(user, censorFilteredMessage);
+                messageResponse = new SendMessage(chatId, description);
                 messageResponse.setParseMode(ParseMode.HTML);
                 DeleteMessage deleteMessageResponse = new DeleteMessage(chatId, message.getMessageId());
                 response.add(deleteMessageResponse);
@@ -137,6 +139,21 @@ public class TextMessageHandlerImpl implements TextMessageHandler {
         var userLang = user.getLanguageCode();
         return String.format("id: %d\nusername: %s\nfirst name: %s\nlast name: %s\nlang: %s",
                 userId, userName, userFirstName, userLastName, userLang);
+    }
+
+    private SendChatAction createTypingChatAction(String chatId) {
+        SendChatAction typingChatAction = new SendChatAction();
+        typingChatAction.setChatId(chatId);
+        typingChatAction.setAction(ActionType.TYPING);
+        return typingChatAction;
+    }
+
+    private String getDescription(User user, String censorFilteredMessage) {
+        return String.format("<code>Оригинальное сообщение от %s %s "
+                + "содержало бранные слова и было изменено:</code>\n\n",
+                user.getFirstName(),
+                user.getLastName() == null ? "" : user.getLastName())
+                + "<i>\"" + censorFilteredMessage + "\"</i>";
     }
 
 }
