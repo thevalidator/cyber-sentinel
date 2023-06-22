6d0581006db2268586f8c59e7b9c39bda74cabb0
thevalidator <the.validator@yandex.ru>
2023-06-23 00:37:16 +0300

    handle edited messages

--- core/src/main/java/ru/thevalidator/core/service/bot/impl/BotServiceImpl.java
+++ core/src/main/java/ru/thevalidator/core/service/bot/impl/BotServiceImpl.java
@@ -7,6 +7,7 @@ package ru.thevalidator.core.service.bot.impl;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -19,6 +20,7 @@ public class BotServiceImpl implements BotService {
     
     private final TextMessageHandler textHadler;
 
+    @Autowired
     public BotServiceImpl(TextMessageHandler textHadler) {
         this.textHadler = textHadler;
     }
@@ -28,13 +30,19 @@ public class BotServiceImpl implements BotService {
     public List<BotApiMethod<? extends Serializable>> getResponse(Update update) {
         
         List<BotApiMethod<? extends Serializable>> response = new ArrayList<>();
-        
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            Message message = update.getMessage();
+        if (hasTextMessage(update) || hasEditedTextMessage(update)) {
+            Message message = hasTextMessage(update) ? update.getMessage() : update.getEditedMessage();
             textHadler.handleTextMessage(message, response);
         }
-        
         return response;
+    }
+    
+    private boolean hasTextMessage(Update update) {
+        return update.hasMessage() && update.getMessage().hasText();
+    }
+    
+    private boolean hasEditedTextMessage(Update update) {
+        return update.hasEditedMessage() && update.getEditedMessage().hasText();
     }
 
 }
